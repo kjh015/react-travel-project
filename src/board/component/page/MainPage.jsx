6523ae3b8dcbf980df2ab2ae2bf44a5c0ee8b276
kjh015 <khj843@naver.com>
2025-06-16 18:47:52 +0900

    9

--- src/board/component/page/MainPage.jsx
+++ src/board/component/page/MainPage.jsx
@@ -14,6 +14,7 @@ const MainPage = () => {
     const evt = new EventSource('http://14.63.178.161:8000/realtime-popular/sse');
     evt.onmessage = (e) => {
       setTop5Board(JSON.parse(e.data));
+      console.log(e.data);
     };
     return () => {
       evt.close();
