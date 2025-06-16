07afa67a31ec59ebde5e015852f01c5576e397cb
kjh015 <khj843@naver.com>
2025-06-16 19:39:55 +0900

    16

--- src/log/components/dashboard/Kibana.jsx
+++ src/log/components/dashboard/Kibana.jsx
@@ -9,19 +9,30 @@ const Kibana = () => {
             padding: 0,
             overflow: 'hidden',
         }}>
-            <iframe
+            <iframe src="http://14.63.178.160:8085/app/dashboards#/view/cda75653-fe83-4bcf-b630-8f11acae0996?embed=false&_g=(refreshInterval:(pause:!t,value:5000),time:(from:now-3h,to:now))&_a=()&show-top-menu=true&show-query-input=true&show-time-filter=true"
+                height="100%"
+                width="100%"
+                style={{
+                    border: 'none',
+                    width: '80vw',
+                    height: '100vh',
+                }}
+                allowFullScreen
+            >
+            </iframe>
+            {/* <iframe
                 src="http://localhost:8085/app/dashboards#/view/a3d1aa29-7d0c-46bc-b512-9a0298cd084c?embed=false&_g=(refreshInterval:(pause:!f,value:5000),time:(from:now-3h,to:now))&_a=()&show-top-menu=true&show-query-input=true&show-time-filter=true"
                 height="100%"
                 width="100%"
                 style={{
                     border: 'none',
                     width: '80vw',
-                    height: '100vh',                    
+                    height: '100vh',
                 }}
                 allowFullScreen>
 
 
-                </iframe>
+            </iframe> */}
 
         </div>
 
