e255218f74f6ce7ff939cf827c7cff17cf566f8d
kjh015 <khj843@naver.com>
2025-06-30 10:02:30 +0900

    14

--- src/board/component/page/MainPage.jsx
+++ src/board/component/page/MainPage.jsx
@@ -182,7 +182,7 @@ const MainPage = () => {
         <div style={{ marginTop: "8rem" }} >
           <h4 style={{
             paddingLeft: "7rem",
-            textAlign: "left",
+            textAlign: "start",
             fontFamily: "'Montserrat', 'Gowun Dodum', sans-serif",
             color: "#b7aaff",
             fontWeight: 700,
